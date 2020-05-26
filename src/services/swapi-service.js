8284ebc8ce78af579dfdc942944539245a7f397d
Drew11 <drew.naumenko@gmail.com>
2020-05-26 17:29:45 +0300

    test

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,10 +1,10 @@
 class SwapiService {
 
-    _url= 'swapi.dev/api';
+    _url= 'https://swapi.dev/api';
 
     async getSource (urlRoute){
 
-        const promise = await fetch('https://'+ this._url + '/' + urlRoute, {
+        const promise = await fetch('https://swapi.dev/api/' + urlRoute, {
             mode:'cors'
         });
 
